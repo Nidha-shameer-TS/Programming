7bb311513657da352474665051face8897d891ff
Nidha-shameer-TS <nidhu.shameer@gmail.com>
2024-08-06 22:21:39 +0530

    first commit

---
+++ area/main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int r,area;
+    printf("Enter the radius of the circle:");
+    scanf("%d",&r);
+    area=3.14*r*r;
+    printf("Area of the circle is : %d",area);
+    return 0;
+}
